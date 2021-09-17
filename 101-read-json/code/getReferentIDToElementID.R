bc8b7b23a37a640b996ce53fe7f693e6c16a5328
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-09-17 13:58:42 -0400

    added: getReferentIDToElementID.R

---
+++ 101-read-json/code/getReferentIDToElementID.R
@@ -0,0 +1,26 @@
+
+getReferentIDToElementID <- function(
+    DF.nested = NULL
+    ) {
+
+    thisFunctionName <- "getReferentIDToElementIDs";
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
+    cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.output <- DF.nested[DF.nested[,'key2'] == 'Referents',];
+    DF.output <- DF.output[DF.output[,'key4'] == 'id',c('key3','value')];
+    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^key3$",  replacement = "ID");
+    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^value$", replacement = "elementID");
+
+    cat("\n# DF.output\n");
+    print(   DF.output   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat(paste0("\n",thisFunctionName,"() quits."));
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
+    return( DF.output );
+
+    }
+
+##################################################
