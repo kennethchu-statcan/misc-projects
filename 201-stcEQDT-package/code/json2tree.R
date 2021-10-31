88812245080c76b19f84ebef40a9ef13a5b9c6cd
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-10-30 20:13:23 -0400

    added: 201-stcEQDT-package/

---
+++ 201-stcEQDT-package/code/json2tree.R
@@ -0,0 +1,101 @@
+#' json2tree
+#'
+#' This function accepts a EQDT JSON file of an electronic questionnaire, and
+#' visualizes the logic structure of the electronic questionnaire in a tree
+#' structure, and prints that tree structure to file.
+#'
+#' @docType class
+#'
+#' @import dplyr
+#' @import R6
+#' @import rjson
+#'
+#' @return NULL
+#'
+#' @examples
+#' # See the vignette 'stcEQDT-usage' for more details, by executing the command: vignette("stcEQDT-usage")
+#'
+#' @param eqdt.json character vector of length 1, containing path to EQDT JSON file
+#' @param file.output character vector of length 1, containing path to output file
+#'
+#' @export
+
+json2tree <- function(
+    eqdt.json   = NULL,
+    file.output = 'EQtree.txt'
+    ) {
+
+    # thisFunctionName <- "EQtree";
+    # cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
+    # cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    list.json        <- getData(input.file = eqdt.json);
+    list.data.frames <- tabularizeData(list.input = list.json);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.QGuid.to.QNumber <- getQGuidToQNumber(
+        DF.nested = list.data.frames[['DF.nested']]
+        );
+    utils::write.csv(file = "DF-QGuid-to-QNumber.csv",   x      = DF.QGuid.to.QNumber, row.names = FALSE);
+    base::saveRDS(  file = "DF-QGuid-to-QNumber.RData", object = DF.QGuid.to.QNumber);
+
+    DF.PGuid.to.PNumber <- getPGuidToPNumber(
+        DF.nested = list.data.frames[['DF.nested']]
+        );
+    utils::write.csv(file = "DF-PGuid-to-PNumber.csv",   x      = DF.PGuid.to.PNumber, row.names = FALSE);
+    base::saveRDS(  file = "DF-PGuid-to-PNumber.RData", object = DF.PGuid.to.PNumber);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    list.referenceID.to.elementID <- getReferenceIDToElementID(
+        DF.nested           = list.data.frames[['DF.nested']],
+        DF.QGuid.to.QNumber = DF.QGuid.to.QNumber,
+        DF.PGuid.to.PNumber = DF.PGuid.to.PNumber
+        );
+    DF.referenceID.to.elementID <- list.referenceID.to.elementID[['referenceID.to.elementID']];
+    DF.referentID.to.elementID  <- list.referenceID.to.elementID[[ 'referentID.to.elementID']];
+
+    utils::write.csv(file = "DF-referenceID-to-elementID.csv",   x      = DF.referenceID.to.elementID, row.names = FALSE);
+    base::saveRDS(  file = "DF-referenceID-to-elementID.RData", object = DF.referenceID.to.elementID);
+
+    utils::write.csv(file = "DF-referentID-to-elementID.csv",   x      = DF.referentID.to.elementID, row.names = FALSE);
+    base::saveRDS(  file = "DF-referentID-to-elementID.RData", object = DF.referentID.to.elementID);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    # DF.element.to.localization <- getElementToLocalization(
+    #     DF.nested                   = list.data.frames[['DF.nested']],
+    #     DF.referenceID.to.elementID = DF.referenceID.to.elementID,
+    #     DF.localization             = list.data.frames[['DF.localization']],
+    #     element.types               = c('datapointValue','displayTarget','gotoTarget','setTarget')
+    #     );
+    # utils::write.csv(file = "DF-element-to-location.csv",   x      = DF.element.to.localization, row.names = FALSE);
+    # base::saveRDS(  file = "DF-element-to-location.RData", object = DF.element.to.localization);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    results.getListOfNodes <- getListOfNodes(
+        list.input                  = list.json,
+        DF.localization             = list.data.frames[['DF.localization']],
+      # DF.element.to.localization  = DF.element.to.localization,
+        DF.referenceID.to.elementID = DF.referenceID.to.elementID,
+        DF.referentID.to.elementID  = DF.referentID.to.elementID
+        );
+
+    DF.nodes   <- results.getListOfNodes[[  'DF.nodes']];
+    list.nodes <- results.getListOfNodes[['list.nodes']];
+
+    utils::write.csv(file = "DF-nodes.csv",     x      =   DF.nodes);
+    base::saveRDS(  file = "DF-nodes.RData",   object =   DF.nodes);
+    base::saveRDS(  file = "list-nodes.RData", object = list.nodes);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    printListOfNodes(
+        list.nodes = list.nodes,
+        txt.output = file.output
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    # cat(paste0("\n",thisFunctionName,"() quits."));
+    # cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
+    return( NULL );
+
+    }
---
+++ 201-stcEQDT-package/code/main.R
@@ -0,0 +1,117 @@
+
+command.arguments <- commandArgs(trailingOnly = TRUE);
+data.directory    <- normalizePath(command.arguments[1]);
+code.directory    <- normalizePath(command.arguments[2]);
+output.directory  <- normalizePath(command.arguments[3]);
+
+print( data.directory );
+print( code.directory );
+print( output.directory );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+start.proc.time <- proc.time();
+
+# set working directory to output directory
+setwd( output.directory );
+
+##################################################
+# source supporting R code
+code.files <- c(
+    "EQtree.R",
+    "getData.R",
+    "getElementToLocalization.R",
+    "getListOfNodes.R",
+    "getPGuidToPNumber.R",
+    "getQGuidToQNumber.R",
+    "getReferenceIDToElementID.R",
+    "node.R",
+    "printListOfNodes.R",
+    "tabularizeData.R"
+    );
+
+for ( code.file in code.files ) {
+    source(file.path(code.directory,code.file));
+    }
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+set.seed(7654321);
+
+list.oidexit <- list(
+    data.snapshot = "2021-05-20.01",
+    file.json     = "OIDEXIT_OID_Exit-Prod-V4-0-0_1-0-0_spec.json",
+    file.output   = "EQtree-oidexit.txt",
+    dir.output    = "EQtree-oidexit"
+    );
+
+list.flow.extraction <- list(
+    data.snapshot = "2021-09-23.01",
+    file.json     = "Flow-Extraction_V1.json",
+    file.output   = "EQtree-flow-extraction.txt",
+    dir.output    = "EQtree-flow-extraction"
+    );
+
+list.industry.classification <- list(
+    data.snapshot = "2021-10-12.01",
+    file.json     = "BCP_ICS_Industry_Classification_Survey.json",
+    file.output   = "EQtree-industry-classification.txt",
+    dir.output    = "EQtree-industry-classification"
+    );
+
+list.CSBC4.2021 <- list(
+    data.snapshot = "2021-10-12.01",
+    file.json     = "CSBC4-2021.json",
+    file.output   = "EQtree-CSBC4-2021.txt",
+    dir.output    = "EQtree-CSBC4-2021"
+    );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+list.of.lists <- list(
+    list.oidexit,
+    list.flow.extraction,
+    list.industry.classification,
+    list.CSBC4.2021
+    );
+
+for ( i in seq(1,length(list.of.lists)) ) {
+
+    list.temp     <- list.of.lists[[i]];
+    data.snapshot <- list.temp[['data.snapshot']];
+    file.json     <- list.temp[['file.json'    ]];
+    file.output   <- list.temp[['file.output'  ]];
+    dir.output    <- list.temp[['dir.output'   ]];
+
+    if ( !dir.exists(dir.output) ) {
+        dir.create(path = dir.output, recursive = TRUE);
+        }
+
+    directory.original <- normalizePath(getwd());
+    setwd(dir.output);
+    Sys.sleep(5);
+
+    EQtree(
+        file.json   = file.path(data.directory,data.snapshot,file.json),
+        file.output = file.output
+        );
+
+    setwd(directory.original);
+    Sys.sleep(5);
+
+    }
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+##################################################
+print( warnings() );
+
+print( getOption('repos') );
+
+print( .libPaths() );
+
+print( sessionInfo() );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+stop.proc.time <- proc.time();
+print( stop.proc.time - start.proc.time );
